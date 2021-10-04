805d8d7bd8fd8739efc2b66b977763a03a6b116d
islahatun <islahatunnufusi98@gmail.com>
2021-10-05 05:58:47 +0700

    Menambahkan Fungtion Profile

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -16,48 +16,9 @@ class Operator extends CI_Controller
         $this->load->view('dashboard/dashboard');
         $this->load->view('templates/footer');
     }
-    public function list_pns()
+    public function profile()
     {
-        //config pagination
-        // $config['base_url'] = 'http://localhost:8080/perpustakaan/Kepala_Pustakawan/list_pustakawan';
-        // $config['total_rows'] = $this->pns->count_pns();
-        // $config['per_page'] = 15;
 
-        //initialize
-        // $this->pagination->initialize($config);
-
-        //styling
-        // $config['full_tag_open'] = '<nav aria-label="Page navigation example"><ul class="pagination">';
-        // $config['full_tag_close'] = '</ul></nav>';
-
-        // $config['first_link'] = 'First';
-        // $config['first_tag_open'] = ' <li class="page-item">';
-        // $config['first_tag_close'] = '</li>';
-
-        // $config['last_link'] = 'Last';
-        // $config['last_tag_open'] = ' <li class="page-item">';
-        // $config['last_tag_close'] = '</li>';
-
-        // $config['next_link'] = '&raqou';
-        // $config['next_tag_open'] = ' <li class="page-item">';
-        // $config['next_tag_close'] = '</li>';
-
-        // $config['next_link'] = '&laqou';
-        // $config['next_tag_open'] = ' <li class="page-item">';
-        // $config['next_tag_close'] = '</li>';
-
-        // $config['cur_tag_open'] = ' <li class="page-item active">';
-        // $config['cur_tag_close'] = '</li>';
-
-        // $config['num_tag_open'] = ' <li class="page-item active">';
-        // $config['num_tag_close'] = '</li>';
-
-        // $config['attributes'] = array('class' => 'page-link');
-
-        //menentukan halaman yang akan dimulai
-        // $data['start'] = $this->uri->segment(3);
-        // $data['list'] = $this->pns->list_pns($config['per_page'], $data['start']);
-        // $data['id'] = $this->pns->getpns();
 
         $data['list'] = $this->pns->getpns();
 
@@ -68,22 +29,5 @@ class Operator extends CI_Controller
         $this->load->view('templates/sidebar');
         $this->load->view('operator/list_pns', $data);
         $this->load->view('templates/footer');
-    }
-    public function insert()
-    {
-        $this->pns->insert_pns();
-
-        redirect('Operator/list_pns');
-    }
-    public function delete($id_pns)
-    {
-        $this->pns->delete_pns($id_pns);
-
-        redirect('Operator/list_pns');
-    }
-    public function update()
-    {
-        $this->pns->update_pns();
-        redirect('Operator/list_pns');
     }
 }
