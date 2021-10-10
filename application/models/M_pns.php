0d74a00bca492c353a07e0970b51aeb3b499bc2d
islahatun <islahatunnufusi98@gmail.com>
2021-10-10 19:04:29 +0700

    Menambahkan update profile di model pns

--- application/models/M_pns.php
+++ application/models/M_pns.php
@@ -73,4 +73,72 @@ class M_pns extends CI_Model
         $this->db->where('nip', $nip);
         $this->db->update('t_pns');
     }
+    public function edit_profile()
+    {
+        $foto = $_FILES['profil']['name'];
+        if ($foto) {
+            $config['upload_path']          = './assets/img/profil/';
+            $config['allowed_types']        = 'gif|jpg|png|jpeg';
+            $config['max_size']             = 2048;
+            $this->load->library('upload', $config);
+            if (!$this->upload->do_upload('profil')) {
+                $error = array('error' => $this->upload->display_errors());
+
+                $this->load->view('upload_form', $error);
+            } else {
+                $new_logo = $this->upload->data('file_name');
+                $n = $this->db->get_where('t_pns', ['nip' => $this->session->userdata('nip')])->row_array();
+                $nip = $n['nip'];
+                // $jk = $this->input->post('jk');
+                // $status = $this->input->post('status');
+
+                $nama = $this->input->post('nama');
+                $sandi = password_hash($this->input->post('sandi'), PASSWORD_DEFAULT);
+                $gelar_depan = $this->input->post('gelar_depan');
+                $gelar_belakang = $this->input->post('gelar_belakang');
+                $jk = $this->input->post('jk');
+                $tmpt_lahir = $this->input->post('tmpt_lahir');
+                $tgl_lahir = $this->input->post('tgl_lahir');
+                $alamat = $this->input->post('alamat');
+                $no_sk_pns = $this->input->post('rt/rw');
+                $pendidikan = $this->input->post('pendidikan');
+                $email = $this->input->post('email');
+                $jabatan = $this->input->post('jabatan');
+                $agama = $this->input->post('agama');
+                $status_kawin = $this->input->post('status_kawin');
+                $penempatan_kerja = $this->input->post('penempatan_kerja');
+                $unit_kerja = $this->input->post('unit_kerja');
+                $pangkat = $this->input->post('pangkat');
+                $gol = $this->input->post('gol');
+                $jurusan = $this->input->post('jurusan');
+                $profil = $new_logo;
+
+
+                $this->db->set('nama', $nama);
+                $this->db->set('sandi', $sandi);
+                $this->db->set('jk', $jk);
+                $this->db->set('gelar_depan', $gelar_depan);
+                $this->db->set('gelar_belakang', $gelar_belakang);
+                $this->db->set('tmpt_lahir', $tmpt_lahir);
+                $this->db->set('tgl_lahir', $tgl_lahir);
+                $this->db->set('email', $email);
+                $this->db->set('alamat', $alamat);
+                $this->db->set('no_sk_pns', $no_sk_pns);
+                $this->db->set('pendidikan', $pendidikan);
+                $this->db->set('jabatan', $jabatan);
+                $this->db->set('agama', $agama);
+                $this->db->set('status_kawin', $status_kawin);
+                $this->db->set('penempatan_kerja', $penempatan_kerja);
+                $this->db->set('unit_kerja', $unit_kerja);
+                $this->db->set('pangkat', $pangkat);
+                $this->db->set('gol', $gol);
+                $this->db->set('jurusan', $jurusan);
+                $this->db->set('profil', $profil);
+                $this->db->where('nik', $nip);
+                $this->db->update('t_pns');
+                // var_dump($data);
+                // die;
+            }
+        }
+    }
 }
